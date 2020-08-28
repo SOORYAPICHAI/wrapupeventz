893016d660f291dd045aa5b0ad1bccdac35a0709
Your Name <jayasoorya@crayond.co>
2020-08-28 21:43:42 +0530

    api changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const Api = "http://localhost:8080/"
+// const Api = "http://localhost:8080/"
 
 // const Api = "http://157.245.101.136:8080/"
 
-// const Api = "https://api.wrapupeventz.com/"
+const Api = "https://api.wrapupeventz.com/"
